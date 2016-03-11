31e2403ae07dfd7f90906d158608d4233285b7b0
Bruno Girin <brunogirin@gmail.com>
2016-03-11 17:07:21 +0000

    Improved topic creation behaviour, added eq and str to topic, str to record

--- interop/GenericDataPlatform/opentrv/data/__init__.py
+++ interop/GenericDataPlatform/opentrv/data/__init__.py
@@ -1,3 +1,8 @@
+import re
+import datetime
+
+DEFAULT_SEPARATOR = '/'
+
 class Record(object):
     def __init__(self, name, ts, value, unit=None, topic=None):
         self.name = name
@@ -6,13 +11,42 @@ class Record(object):
         self.unit = unit
         self.topic = topic
 
-class Topic(object):
-    def __init__(self, name, parent=None):
-        self.name = name
-        self.parent = parent
+    def __str__(self):
+        return "[{0}] {1}@{2} {3}{4}".format(
+            "" if self.topic is None else self.topic,
+            self.name,
+            int((self.timestamp - datetime.datetime.utcfromtimestamp(0)).total_seconds()),
+            self.value,
+            "" if self.unit is None else " {0}".format(self.unit)
+            )
 
-    def path(self, sep='/'):
+class Topic(object):
+    def __init__(self, name, parent=None, sep=DEFAULT_SEPARATOR):
+        while name[0] == sep:
+            name = name[1:]
+        while name[-1] == sep:
+            name = name[:-1]
+        c = name.rfind(sep)
+        if c >= 0:
+            self.name = name[c+1:]
+            self.parent = Topic(name[:c], parent, sep)
+        else:
+            self.name = name
+            self.parent = parent
+
+    def path(self, sep=DEFAULT_SEPARATOR):
         if self.parent is None:
             return self.name
         else:
             return sep.join([self.parent.path(), self.name])
+
+    def __eq__(self, other):
+        return (
+            self.name == other.name and
+            (other.parent is None) if self.parent is None else (
+                other.parent is not None and other.parent == self.parent
+                )
+            )
+
+    def __str__(self):
+        return self.path()
--- interop/GenericDataPlatform/test/data/test_record.py
+++ interop/GenericDataPlatform/test/data/test_record.py
@@ -1,15 +1,72 @@
 import unittest
+import datetime
 
 import opentrv.data
 
 class TestRecord(unittest.TestCase):
     def test_init(self):
-        pass
+        ts = datetime.datetime.utcnow()
+        r = opentrv.data.Record("t", ts, 10)
+        self.assertEqual("t", r.name)
+        self.assertEqual(ts, r.timestamp)
+        self.assertEqual(10, r.value)
+        self.assertIsNone(r.unit)
+        self.assertIsNone(r.topic)
+
+    def test_str_basic(self):
+        ts = datetime.datetime.now()
+        ts_str = str(int((ts - datetime.datetime.utcfromtimestamp(0)).total_seconds()))
+        r = opentrv.data.Record("t", ts, 10)
+        self.assertEqual("[] t@"+ts_str+" 10", str(r))
+
+    def test_str_full(self):
+        ts = datetime.datetime.now()
+        ts_str = str(int((ts - datetime.datetime.utcfromtimestamp(0)).total_seconds()))
+        r = opentrv.data.Record("t", ts, 10, "W", opentrv.data.Topic("topic"))
+        self.assertEqual("[topic] t@"+ts_str+" 10 W", str(r))
 
 class TestTopic(unittest.TestCase):
     def test_init(self):
         t = opentrv.data.Topic("mytopic")
         self.assertEqual("mytopic", t.name)
+
+    def test_init_complex_name(self):
+        t = opentrv.data.Topic("my/topic")
+        self.assertEqual("topic", t.name)
+        self.assertIsNotNone(t.parent)
+        self.assertEqual("my", t.parent.name)
+
+    def test_init_complex_name_prefix(self):
+        t = opentrv.data.Topic("//my/topic")
+        self.assertEqual("topic", t.name)
+        self.assertIsNotNone(t.parent)
+        self.assertEqual("my", t.parent.name)
+
+    def test_init_complex_name_suffix(self):
+        t = opentrv.data.Topic("my/topic//")
+        self.assertEqual("topic", t.name)
+        self.assertIsNotNone(t.parent)
+        self.assertEqual("my", t.parent.name)
+
+    def test_init_complex_name_double_sep(self):
+        t = opentrv.data.Topic("my//topic")
+        self.assertEqual("topic", t.name)
+        self.assertIsNotNone(t.parent)
+        self.assertEqual("my", t.parent.name)
+
+    def test_init_complex_name_custom_sep(self):
+        t = opentrv.data.Topic("my.topic", sep='.')
+        self.assertEqual("topic", t.name)
+        self.assertIsNotNone(t.parent)
+        self.assertEqual("my", t.parent.name)
+
+    def test_init_complex_name_with_parent(self):
+        t = opentrv.data.Topic("my/topic", opentrv.data.Topic("oh"))
+        self.assertEqual("topic", t.name)
+        self.assertIsNotNone(t.parent)
+        self.assertEqual("my", t.parent.name)
+        self.assertIsNotNone(t.parent.parent)
+        self.assertEqual("oh", t.parent.parent.name)
 
     def test_path_single(self):
         t = opentrv.data.Topic("mytopic")
@@ -24,6 +81,40 @@ class TestTopic(unittest.TestCase):
         t1 = opentrv.data.Topic("one")
         t2 = opentrv.data.Topic("two", t1)
         self.assertEqual("one.two", t2.path('.'))
+
+    def test_eq(self):
+        t1 = opentrv.data.Topic("topic")
+        t2 = opentrv.data.Topic("topic")
+        self.assertEqual(t1, t2)
+
+    def test_eq_with_parent(self):
+        t1 = opentrv.data.Topic("topic", opentrv.data.Topic("parent"))
+        t2 = opentrv.data.Topic("topic", opentrv.data.Topic("parent"))
+        self.assertEqual(t1, t2)
+
+    def test_ne(self):
+        t1 = opentrv.data.Topic("topic")
+        t2 = opentrv.data.Topic("other")
+        self.assertNotEqual(t1, t2)
+
+    def test_ne_with_parent(self):
+        t1 = opentrv.data.Topic("topic", opentrv.data.Topic("parent"))
+        t2 = opentrv.data.Topic("topic", opentrv.data.Topic("other"))
+        self.assertNotEqual(t1, t2)
+
+    def test_ne_with_one_parent_other(self):
+        t1 = opentrv.data.Topic("topic", opentrv.data.Topic("parent"))
+        t2 = opentrv.data.Topic("topic")
+        self.assertNotEqual(t1, t2)
+
+    def test_ne_with_one_parent_self(self):
+        t1 = opentrv.data.Topic("topic")
+        t2 = opentrv.data.Topic("topic", opentrv.data.Topic("parent"))
+        self.assertNotEqual(t1, t2)
+
+    def test_str(self):
+        t = opentrv.data.Topic("topic", opentrv.data.Topic("parent"))
+        self.assertEqual(t.path(), str(t))
 
 if __name__ == '__main__':
     unittest.main()
