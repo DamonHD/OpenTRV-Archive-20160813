6148231ac080d7f3f5b6e814c940604c0de8b7ac
Bruno Girin <brunogirin@gmail.com>
2016-03-15 16:12:12 +0000

    Moved app code into sub-package to avoid flash dependencies when testing model

--- interop/GenericDataPlatform/opentrv/platform/__main__.py
+++ interop/GenericDataPlatform/opentrv/platform/__main__.py
@@ -1,11 +1,11 @@
 import sys
 import logging
-import opentrv.platform
+import opentrv.platform.app
 
 # Main entry into the system. This code is called when the platform
 # module is run from the command line using the python -m option.
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     logger.info("Starting platform")
-    app = opentrv.platform.app
+    app = opentrv.platform.app.app
     app.run(debug=True)
