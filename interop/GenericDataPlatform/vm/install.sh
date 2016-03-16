0ed9b2c54a97e6a4e7efe5682f477d8efc8320b9
Bruno Girin <brunogirin@gmail.com>
2016-03-16 18:10:21 +0000

    Updated install script to handle install on non-vagrant system (e.g. on a Pi)

--- interop/GenericDataPlatform/vm/install.sh
+++ interop/GenericDataPlatform/vm/install.sh
@@ -5,6 +5,10 @@ set -e
 # Load up the release information
 . /etc/lsb-release
 
+f0=$(readlink -e $0)
+d0=$(dirname $f0)
+d1=$(dirname $d0)
+
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing PIP and Mosquitto"
@@ -16,7 +20,11 @@ apt-get install -qy mosquitto-clients
 
 echo "Installing python packages"
 if [ -d /vagrant ]; then
+    # if in vagrant VM then install as editable
     pip3 install -r /vagrant/vm/requirements.txt
+    pip3 install -e /vagrant
 else
-    pip2 install -r ./requirements.txt
+    # if not in vagrant VM then install from local as non-editable
+    pip3 install -r $d0/requirements.txt
+    pip3 install $d1
 fi
