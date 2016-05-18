526ed0bf3066286e8fc7f71e4687b256546176fa
DamonHD <d@hd.org>
2016-05-18 09:06:12 +0100

    TODO-897: WIP

--- javasrc/uk/org/opentrv/hdd/MeterReadingsExtractor.java
+++ javasrc/uk/org/opentrv/hdd/MeterReadingsExtractor.java
@@ -22,11 +22,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Collections;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 
-/**Extracts energy meter readings from an ASCII CSV reader/stream.
+/**Extracts energy meter readings as cumulative use from an ASCII CSV reader/stream.
  * This expects an input where the first column is (or starts with)
  * a YYYY-MM-DD or YYYY/MM/DD date
  * and the second column is an (ascending) meter reading.
@@ -34,7 +35,11 @@ import java.util.TreeMap;
  * <p>
  * This will reject input where values go down at any point,
  * unless given specific instructions how to handle such elements,
- * eg due to meter roll-over or meter replacement.
+ * eg due to meter roll-over or meter replacement,
+ * or given non-cumulative style inputs eg per interval readings
+ * in which case a cumulative output will be generated.
+ * <p>
+ * This rejects all negative meter values.
  * <p>
  * This does not attempt to interpret units.
  * <p>
@@ -87,10 +92,24 @@ public final class MeterReadingsExtractor
      * <p>
      * Does NOT close the Reader.
      *
-     * @return immutable non-null map of monotonically-increasing readings by date
+     * @return immutable non-null map of non-strictly monotonically-increasing readings by date
      * @throws IOException in case of parse error or missing or ambiguous or non-monotonic data
      */
     public static SortedMap<Integer, Double> extractMeterReadings(final Reader r)
+        throws IOException
+        { return(extractMeterReadings(r, false)); }
+
+
+    /**Extract meter readings from CSV as map from YYYYMMDD date key to reading as supplied; never null.
+     * @param nonCumulativeInput if false rejects data where readings go down over time,
+     *     else rejects negative values and converts to cumulative
+     * <p>
+     * Does NOT close the Reader.
+     *
+     * @return immutable non-null map of non-strictly monotonically-increasing readings by date
+     * @throws IOException in case of parse error or missing or ambiguous or non-monotonic data
+     */
+    public static SortedMap<Integer, Double> extractMeterReadings(final Reader r, final boolean nonCumulativeInput)
         throws IOException
         {
         if(null == r) { throw new IllegalArgumentException(); }
@@ -98,8 +117,12 @@ public final class MeterReadingsExtractor
         // Wrap in BufferedReader if required.
         final BufferedReader br = (r instanceof BufferedReader) ? ((BufferedReader) r) : new BufferedReader(r);
 
+        // Map of raw parsed values.
         final SortedMap<Integer, Double> m = new TreeMap<>();
+        // Map of values converted to cumulative; will be a separate map for non-cumulative inputs.
+        final SortedMap<Integer, Double> mCumulative = nonCumulativeInput ? new TreeMap<>() : m;
 
+        // Note that the values may be presented in any date order.
         String line;
         while(null != (line = br.readLine()))
             {
@@ -110,7 +133,9 @@ public final class MeterReadingsExtractor
             final int day;
             final Integer key;
             final String d = fields[0];
-            final Double reading;
+            final Double rawReading;
+
+            // Parse date.
             if((10 > d.length()) || ('-' != d.charAt(4)) || ('-' != d.charAt(7)))
                 { continue; }
             try
@@ -118,15 +143,38 @@ public final class MeterReadingsExtractor
                 year = Integer.parseInt(d.substring(0, 4), 10);
                 month = Integer.parseInt(d.substring(5, 7), 10);
                 day = Integer.parseInt(d.substring(8), 10);
-                reading = Double.parseDouble(fields[1]);
+                rawReading = Double.parseDouble(fields[1]);
                 }
             catch(final NumberFormatException e)
                 { continue; } // Skip bad line.
-            if(Double.isNaN(reading)) { continue; } // Skip bad line.
-            if(Double.isInfinite(reading)) { continue; } // Skip bad line.
             key = (year * 10000) + (month * 100) + day;
-            m.put(key, reading);
+
+            // Parse reading.
+            // Unparseable readings can be skipped and possibly recovered from if input is cumulative.
+            // For non-cumulative input no input errors can be recovered from.
+            if(Double.isNaN(rawReading) ||
+               Double.isInfinite(rawReading) ||
+               (rawReading < 0))
+                {
+                if(nonCumulativeInput) { throw new IOException("bad meter reading at " + key); }
+                continue;
+                }
+
+            m.put(key, rawReading);
             }
+
+        // Populate cumulative version of map if necessary.
+        if(nonCumulativeInput)
+            {
+            assert(mCumulative.isEmpty());
+            double sum = 0;
+            for(final Map.Entry<Integer, Double> e : m.entrySet())
+                {
+                sum += e.getValue().doubleValue();
+                mCumulative.put(e.getKey(), sum);
+                }
+            }
+
         // Ensure that values increase monotonically with date,
         // though not strictly since successive values can be the same.
         Double prev = null;
@@ -137,7 +185,7 @@ public final class MeterReadingsExtractor
             prev = v;
             }
 
-        return(Collections.unmodifiableSortedMap(m));
+        return(Collections.unmodifiableSortedMap(mCumulative));
         }
 
     }
