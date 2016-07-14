38a7b3c6c7071a373126f0900dd19a8d94b9a55d
DamonHD <d@hd.org>
2016-07-14 09:41:42 +0100

    TODO-897: ETVPerHouseholdComputationSimpleImpl start

---
+++ javasrc/uk/org/opentrv/ETV/ETVPerHouseholdComputationSimpleImpl.java
@@ -0,0 +1,18 @@
+package uk.org.opentrv.ETV;
+
+/**Simple computation implementation for one household, no efficacy.
+ * This can do a simple computation to find overall kWh/HDD
+ * from the supplied house's data,
+ * ignoring any change in efficiency with equipment operation.
+ */
+public class ETVPerHouseholdComputationSimpleImpl implements ETVPerHouseholdComputation
+    {
+
+    @Override
+    public ETVPerHouseholdComputationResult compute(ETVPerHouseholdComputationInput in) throws IllegalArgumentException
+        {
+        // TODO Auto-generated method stub
+        return null;
+        }
+
+    }
